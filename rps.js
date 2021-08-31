f3f666121d1cb1591999d11f5c58134cf56f0ca6
VirginiaWilliams <ginnyglink@gmail.com>
2021-08-31 15:23:00 -0400

    still trying to figure out js

--- rps.js
+++ rps.js
@@ -1,9 +1,10 @@
-let randnum = Math.floor(Math.random()) + 1;
-
-const playSubmit = document.querySelector(".submit");
-
-function hello() {
-  console.log("hello");
-}
-
-playSubmit.addEventListener("click", hello);
+// let randnum = Math.floor(Math.random()) + 1;
+//
+// const playSubmit = document.querySelector(".submit");
+//
+// function hello() {
+//   console.log("hello");
+// }
+//
+// playSubmit.addEventListener("click", hello);
+alert("hello");
