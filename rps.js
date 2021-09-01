615488566d195817d22ed3bfc28306d0b0da5ace
VirginiaWilliams <ginnyglink@gmail.com>
2021-09-01 12:02:01 -0400

    again

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ let randnum = Math.floor(Math.random()) + 1;
 const playSubmit = document.querySelector(".submit");
 
 function computerPlay(num) {
-  console.log(num%3);
+  alert(num%3);
   return num % 3;
 }
 
